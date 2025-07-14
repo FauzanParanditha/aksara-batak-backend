5cd02517bf62e10c500f84dda31ee81dad28060c
Fauzan Paranditha <paranditha@gmail.com>
2025-07-14 08:46:58 +0700

    refactore

--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PaymentStatus, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getDashboardStatsLeader = async (userId: string) => {
@@ -24,12 +24,43 @@ export const getDashboardStatsLeader = async (userId: string) => {
 };
 
 export const getDashboardStatsAdmin = async () => {
-  const [totalTeams, totalMember] = await Promise.all([
+  const [
+    totalTeams,
+    totalMember,
+    totalLeader,
+    waitingPayment,
+    paidPayment,
+    submission,
+  ] = await Promise.all([
     prisma.team.count(),
     prisma.teamMember.count(),
+    prisma.user
+      .findMany({
+        where: { role: "leader" },
+      })
+      .then((users) => users.length),
+    prisma.payment
+      .findMany({
+        where: { status: PaymentStatus.waiting_verification },
+      })
+      .then((payments) => payments.length),
+    prisma.payment
+      .findMany({
+        where: { status: PaymentStatus.paid },
+      })
+      .then((payments) => payments.length),
+    prisma.team
+      .findMany({
+        where: { submissionLink: { not: null } },
+      })
+      .then((teams) => teams.length),
   ]);
   return {
     totalTeams,
     totalMember,
+    totalLeader,
+    waitingPayment,
+    paidPayment,
+    submission,
   };
 };
